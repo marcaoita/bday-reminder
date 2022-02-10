9c9d2f6705b09660e95ed32f7c4d7f54c2e0f6bd
Marcos <marcosguo@gmail.com>
2022-02-09 23:48:52 -0300

    Second commit @Override

--- app/src/main/java/com/list/agendalist/MainActivity.java
+++ app/src/main/java/com/list/agendalist/MainActivity.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class MainActivity extends Activity{
 
-
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
